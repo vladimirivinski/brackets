f85369ebb4caa5612921cb665ff51285c2f7ff03
Vladimir.Ivinski <64367581+vladimirivinski@users.noreply.github.com>
2021-02-01 00:59:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let storeBrackets = [],
+        pairOfBrackets = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        pairOfBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    let strToArr = str.split("");
+    for (let i = 0; i < strToArr.length; i++) {
+        if (strToArr[i] === (storeBrackets[storeBrackets.length - 1] || 0)) {
+            storeBrackets.pop();
+        } else {
+            storeBrackets.push(pairOfBrackets[strToArr[i]]);
+        }
+    }
+    if (storeBrackets.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
